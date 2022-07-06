104e301e9895062f017f3e38dd313f4f93ced615
Smooke09 <pedrolucas1014zz@gmail.com>
2022-07-06 11:50:00 -0300

    content component

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default (props) => {
+  return (
+    <Main
+      icon="home"
+      title="Inicio"
+      subtitle="Segundo Projeto do capitulo de React."
+    >
+      <div className="display-4">Bem Vindo!</div>
+      <hr />
+      <p className="mb-0">
+        Sistema para exemplificar a construção de um cadastro desenvolvido em
+        React
+      </p>
+    </Main>
+  );
+};
